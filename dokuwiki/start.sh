25bf89bbea2323baff6767bf5b25c3831299db88
nickrobinson <nrobinson13@gmail.com>
2019-12-30 09:24:09 -0500

    bug: fix initialization issue with dokuwiki

--- dokuwiki/start.sh
+++ dokuwiki/start.sh
@@ -1,25 +1,41 @@
 #!/bin/sh
 
-FILE=/tmp/initialized
+FILE=/var/dokuwiki-storage/conf/manifest.json
 
 set -e
 
-if [ ! -f "$FILE" ]; then
+if test -f "$FILE"; then
+  echo "Found files"
+  rm -fr /var/www/data/pages
+  rm -fr /var/www/data/meta
+  rm -fr /var/www/data/media
+  rm -fr /var/www/data/media_attic
+  rm -fr /var/www/data/media_meta
+  rm -fr /var/www/data/attic
+  rm -fr /var/www/conf
+  ln -s /var/dokuwiki-storage/data/pages/ /var/www/data/pages
+  ln -s /var/dokuwiki-storage/data/meta/ /var/www/data/meta
+  ln -s /var/dokuwiki-storage/data/media/ /var/www/data/media
+  ln -s /var/dokuwiki-storage/data/media_attic/ /var/www/data/media_attic
+  ln -s /var/dokuwiki-storage/data/media_meta/ /var/www/data/media_meta
+  ln -s /var/dokuwiki-storage/data/attic/ /var/www/data/attic
+  ln -s /var/dokuwiki-storage/conf/ /var/www/conf
+else
+  echo "Did NOT find files"
   mv -n /var/www/data/pages /var/dokuwiki-storage/data/pages && \
-  ln -s /var/dokuwiki-storage/data/pages /var/www/data/pages && \
+  ln -s /var/dokuwiki-storage/data/pages/ /var/www/data/pages && \
   mv -n /var/www/data/meta /var/dokuwiki-storage/data/meta && \
-  ln -s /var/dokuwiki-storage/data/meta /var/www/data/meta && \
+  ln -s /var/dokuwiki-storage/data/meta/ /var/www/data/meta && \
   mv -n /var/www/data/media /var/dokuwiki-storage/data/media && \
-  ln -s /var/dokuwiki-storage/data/media /var/www/data/media && \
+  ln -s /var/dokuwiki-storage/data/media/ /var/www/data/media && \
   mv -n /var/www/data/media_attic /var/dokuwiki-storage/data/media_attic && \
-  ln -s /var/dokuwiki-storage/data/media_attic /var/www/data/media_attic && \
+  ln -s /var/dokuwiki-storage/data/media_atti/c /var/www/data/media_attic && \
   mv -n /var/www/data/media_meta /var/dokuwiki-storage/data/media_meta && \
-  ln -s /var/dokuwiki-storage/data/media_meta /var/www/data/media_meta && \
+  ln -s /var/dokuwiki-storage/data/media_meta/ /var/www/data/media_meta && \
   mv -n /var/www/data/attic /var/dokuwiki-storage/data/attic && \
-  ln -s /var/dokuwiki-storage/data/attic /var/www/data/attic && \
+  ln -s /var/dokuwiki-storage/data/attic/ /var/www/data/attic && \
   mv -n /var/www/conf /var/dokuwiki-storage/conf && \
-  ln -s /var/dokuwiki-storage/conf /var/www/conf
-  touch /tmp/initialized
+  ln -s /var/dokuwiki-storage/conf/ /var/www/conf
 fi
 
 
